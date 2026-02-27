2aebbeecc39d1df61dc6cbba5e5b085913aea6f3
Bwezani Kanyimbi <kanyimbib@gmail.com>
2026-02-27 06:41:35 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7970143774-96b27",
+  "appId": "1:951261368695:web:59574ac1301dea71e35172",
+  "apiKey": "AIzaSyCefQg7Lq74XPMJYDIFbnc-oxWWxylSiio",
+  "authDomain": "studio-7970143774-96b27.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "951261368695"
+};
